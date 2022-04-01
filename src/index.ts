688cbc86d1b4d191a056729b209da16c6fc3f23e
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-04-01 21:40:19 +0900

    fix: update template

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import company from "src/company.json";
-
+const main = () => {
+  console.log(`Hello, world!!`);
+};
 main();
-function main() {
-  console.log(`This is ${company.name}`);
-  company.members.forEach(member => {
-    console.log(`name: ${member.name}, age: ${member.age}`);
-  });
-}
